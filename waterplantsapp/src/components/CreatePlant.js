60491402491ec7f0dbb694f0d209bdc489aabcaa
thomas <t_huynh927@yahoo.com>
2020-03-05 20:01:39 -0800

    styled createplant and updateplant

--- waterplantsapp/src/components/CreatePlant.js
+++ waterplantsapp/src/components/CreatePlant.js
@@ -1,53 +1,88 @@
 import React, { useState } from 'react';
-import { withRouter } from 'react-router';
 import { axiosWithAuth } from '../authorization/axiosWithAuth.js';
+import styled from "styled-components";
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  margin: 0 auto;
+  align-items: center;
+  label {
+    margin-top: 2rem;
+    font-size: 2rem;
+  }
+
+  input {
+    width: 30rem;
+    padding: 1rem 0.25rem;
+    border-radius: 0.5rem;
+    font-size: 2rem;
+  }
+`
+
+const SubmitButton = styled.button`
+  width: 20rem;
+  border: none;
+  padding: 1.5rem 0.5rem;
+  border-radius: 0.5rem;
+  margin-top: 2rem;
+  transition: background 300ms ease-in;
+  &:hover {
+    background: rgba(43, 48, 58, 0.9);
+    transition: background 300ms ease-in;
+    color: white;
+  }
+  &:active {
+    background: orange;
+  }
+`
 
 const CreatePlant = (props) => {
- const [plant, setPlant] = useState({name: "", species: "", h20frequency: ""});
+  const [plant, setPlant] = useState({ name: "", species: "", h20frequency: "" });
 
   const create = e => {
     e.preventDefault();
     axiosWithAuth()
-        .post("https://water-myplants-2.herokuapp.com/api/plants", plant)
-        .then(res => props.history.push('/'))
-        .catch(err => console.log(err));
+      .post("https://water-myplants-2.herokuapp.com/api/plants", plant)
+      .then(res => props.history.push('/'))
+      .catch(err => console.log(err));
   }
 
   const handleChange = e => {
-      setPlant({
-        ...plant,
-        [e.target.name]: e.target.value,
-      })
+    setPlant({
+      ...plant,
+      [e.target.name]: e.target.value,
+    })
   }
 
-    return (
-      <div>
-        <form onSubmit={create}>
-            <label>Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={plant.name}
-            onChange={handleChange}
-          />
-          <label>Species: </label>
-          <input
-            type="text"
-            name="species"
-            value={plant.species}
-            onChange={handleChange}
-          />
-          <label>H20frequency: </label>
-          <input
-            type="number"
-            name="h20frequency"
-            value={plant.h20frequency}
-            onChange={handleChange}
-          />
-          <button>Create Plant</button>
-        </form>
-      </div>
-    )
+  return (
+    <Form onSubmit={create}>
+      <h2>Create your plant!</h2>
+      <label>Name </label>
+      <input
+        type="text"
+        name="name"
+        value={plant.name}
+        onChange={handleChange}
+      />
+      <label>Species </label>
+      <input
+        type="text"
+        name="species"
+        value={plant.species}
+        onChange={handleChange}
+      />
+      <label>H20frequency </label>
+      <input
+        type="number"
+        name="h20frequency"
+        value={plant.h20frequency}
+        onChange={handleChange}
+      />
+      <SubmitButton>Create Plant</SubmitButton>
+    </Form>
+  )
 }
 
 export default CreatePlant;
--- waterplantsapp/src/components/UpdatePlant.js
+++ waterplantsapp/src/components/UpdatePlant.js
@@ -1,14 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import { axiosWithAuth } from '../authorization/axiosWithAuth.js';
+import styled from "styled-components";
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  margin: 0 auto;
+  align-items: center;
+  label {
+    margin-top: 2rem;
+    font-size: 2rem;
+  }
+
+  input {
+    width: 30rem;
+    padding: 1rem 0.25rem;
+    border-radius: 0.5rem;
+    font-size: 2rem;
+  }
+`
+
+const SubmitButton = styled.button`
+  width: 20rem;
+  border: none;
+  padding: 1.5rem 0.5rem;
+  border-radius: 0.5rem;
+  margin-top: 2rem;
+  transition: background 300ms ease-in;
+  &:hover {
+    background: rgba(43, 48, 58, 0.9);
+    transition: background 300ms ease-in;
+    color: white;
+  }
+  &:active {
+    background: orange;
+  }
+`
 
 const UpdatePlant = (props) => {
     console.log(props.location);
  const [plant, setPlant] = useState({name: "", species: "", h20frequency: ""});
 
-useEffect(() => {
-    setPlant(props.location.updateProps);
-}, [props.location.updateProps])
+  useEffect(() => {
+      setPlant(props.location.updateProps);
+  }, [props.location.updateProps])
 
   const update = e => {
 
@@ -30,32 +67,31 @@ useEffect(() => {
   }
 
     return (
-      <div>
-        <form onSubmit={update}>
-            <label>Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={plant.name}
-            onChange={handleChange}
-          />
-          <label>Species: </label>
-          <input
-            type="text"
-            name="species"
-            value={plant.species}
-            onChange={handleChange}
-          />
-          <label>H20Frequency: </label>
-          <input
-            type="number"
-            name="h20frequency"
-            value={plant.h2ofrequency}
-            onChange={handleChange}
-          />
-          <button>Update plant</button>
-        </form>
-      </div>
+      <Form onSubmit={update}>
+        <h2>Update your plant!</h2>
+          <label>Name </label>
+        <input
+          type="text"
+          name="name"
+          value={plant.name}
+          onChange={handleChange}
+        />
+        <label>Species </label>
+        <input
+          type="text"
+          name="species"
+          value={plant.species}
+          onChange={handleChange}
+        />
+        <label>H20Frequency </label>
+        <input
+          type="number"
+          name="h20frequency"
+          value={plant.h2ofrequency}
+          onChange={handleChange}
+        />
+        <SubmitButton>Update plant</SubmitButton>
+      </Form>
     )
 }
 
